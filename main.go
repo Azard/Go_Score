c6a6295aeaba2f0d3be45f0315e8a31d27557db4
熊伟伦 <330815461@qq.com>
2014-08-20 01:38:28 +0800

    Add redirect

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 )
 
 const (
-	URL_Favicon string = "./static/ico/favicon.ico"
-	URL_Login   string = "./static/login.gtpl"
-	URL_Client  string = "./static/client.gtpl"
+	Path_Favicon string = "./static/ico/favicon.ico"
+	Path_Login   string = "./static/login.gtpl"
+	Path_Student string = "./static/student.gtpl"
 )
 
 func console_show_Req(r *http.Request) {
@@ -27,22 +27,40 @@ func serveSingle(pattern string, filename string) {
 	})
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func login(w http.ResponseWriter, r *http.Request) {
 	console_show_Req(r)
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles(URL_Login)
+		t, _ := template.ParseFiles(Path_Login)
 		t.Execute(w, nil)
 	} else if r.Method == "POST" {
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
-		t, _ := template.ParseFiles(URL_Client)
-		t.Execute(w, nil)
+		//验证用户名密码
+		if r.Form["username"][0] == "admin" && r.Form["password"][0] == "admin" {
+			http.Redirect(w, r, "/student", http.StatusFound)
+			return //http.Redirect会执行后面的代码，return保证安全
+		} else { // 登陆失败
+			t, _ := template.ParseFiles(Path_Login)
+			t.Execute(w, nil)
+		}
 	}
 }
 
+func student(w http.ResponseWriter, r *http.Request) {
+	console_show_Req(r)
+	t, _ := template.ParseFiles(Path_Student)
+	t.Execute(w, nil)
+}
+
+func admin(w http.ResponseWriter, r *http.Request) {
+	console_show_Req(r)
+}
+
 func main() {
-	http.HandleFunc("/", home)
-	serveSingle("/favicon.ico", URL_Favicon)
+	http.HandleFunc("/", login)
+	http.HandleFunc("/student", student)
+	http.HandleFunc("/admin", admin)
+	serveSingle("/favicon.ico", Path_Favicon)
 
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
